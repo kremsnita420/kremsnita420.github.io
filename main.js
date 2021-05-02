7ad76264f39930ac02ede27a77749c8571309153
kremsnita420 <35669905+kremsnita420@users.noreply.github.com>
2021-05-02 21:56:40 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+window.onload = () => {
+    setTimeout(() => {
+      document.querySelector("body").classList.add("display");
+    }, 4000);
+  };
+
+/*TOGGLE HAMBURGER MENU */
+
+    document.querySelector(".hamburger-menu").
+    addEventListener("click", () => {
+        document.querySelector(".container").classList.toggle
+        ("change");
+    });
+
+/*SCROLL UP BUTTON */
+    document.querySelector(".scroll-btn").addEventListener("click", () => {
+        document.querySelector("html").style.scrollBehavior = "smooth";
+        setTimeout(() => {
+        document.querySelector("html").style.scrollBehavior = "unset";
+        }, 1000);
+    });
